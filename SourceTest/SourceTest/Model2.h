e40d1eaeb379446f0d0cdaab19fbd260686cf146
刘洪君 <liuhongjun@liuhongkuntekiMacBook-Pro.local>
2017-02-09 12:50:15 +0800

    add Model2

---
+++ SourceTest/SourceTest/Model2.h
@@ -0,0 +1,13 @@
+//
+//  Model2.h
+//  SourceTest
+//
+//  Created by  刘洪君 on 17/2/9.
+//  Copyright © 2017年 HJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model2 : NSObject
+
+@end
---
+++ SourceTest/SourceTest/Model2.m
@@ -0,0 +1,13 @@
+//
+//  Model2.m
+//  SourceTest
+//
+//  Created by  刘洪君 on 17/2/9.
+//  Copyright © 2017年 HJ. All rights reserved.
+//
+
+#import "Model2.h"
+
+@implementation Model2
+
+@end
